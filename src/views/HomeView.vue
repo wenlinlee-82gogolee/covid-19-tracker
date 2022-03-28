0c2f3057b4e47452102490a5d378c50ec19668a6
Wen-Lin Lee <82gogolee@gmail.com>
2022-03-28 13:37:47 +0800

    add CountrySelect component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,15 @@
   <main v-if="!loading">
     <DataTitle :text="title" :dataDate="dataDate" />
     <DataBoxes :stats="stats" />
+    <CountrySelect :countries="countries" @get-country="getCountryData" />
+
+    <button
+      @click="clearCountryData"
+      class="bg-blue-500 text-white rounded p-3 mt-10 mb-10 focus:outline-none hover:bg-blue-400"
+      v-if="stats.Country"
+    >
+      Clear Country
+    </button>
   </main>
   <main v-else class="flex flex-col align-center justify-center text-center">
     <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching Data...</div>
@@ -12,9 +21,10 @@
 <script>
 import DataTitle from '../components/DataTitile.vue';
 import DataBoxes from '../components/DataBoxes.vue';
+import CountrySelect from '../components/CountrySelect.vue';
 export default {
   name: 'HomeView',
-  components: { DataTitle, DataBoxes },
+  components: { DataTitle, DataBoxes, CountrySelect },
   data() {
     return {
       loading: true,
@@ -30,6 +40,17 @@ export default {
       const res = await fetch('https://api.covid19api.com/summary');
       const data = await res.json();
       return data;
+    },
+    getCountryData(country) {
+      this.stats = country;
+      this.title = country.Country;
+    },
+    async clearCountryData() {
+      this.loading = true;
+      const data = await this.fetchCovidData();
+      this.title = 'Global';
+      this.stats = data.Global;
+      this.loading = false;
     },
   },
   async created() {
